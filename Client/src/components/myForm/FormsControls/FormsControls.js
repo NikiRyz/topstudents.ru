fd01063f2e70b1f5a1ea61bd923aec08f795a5c4
boumrz <boumrz@gmail.com>
2019-11-29 10:52:27 +0300

    release topstudents

--- Client/src/components/myForm/FormsControls/FormsControls.js
+++ Client/src/components/myForm/FormsControls/FormsControls.js
@@ -6,9 +6,6 @@ export const Input = ({input, meta, ...props }) => {
   return (
     <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
       <div>
-        {
-          console.log(input)
-        }
         <input {...input} {...props} />
       </div>
       {hasError && <span>{meta.error}</span>}
